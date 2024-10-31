e7afe9e7f0517e88f11525bee58902e5f965b0ef
liuchiawei <doublecheap@gmail.com>
2024-10-31 11:24:01 +0900

    header title and footer

--- include/head.php
+++ include/head.php
@@ -1,4 +1,5 @@
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <title>DEEP SHINJUKU - 新宿怪談集</title>
 </head>
