c2cec74e71e20d9dd38cb6de6dae9a4ed7ba8d3b
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2024-10-31 09:58:59 +0900

    update

---
+++ function/update_likes.php
@@ -0,0 +1,10 @@
+<?php
+$jsonFile = '/data/stories.json';
+$jsonData = file_get_contents($jsonFile);
+$data = json_decode($jsonData, true);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   $data['likes'] = (int)$data['likes'] + 1;
+   file_put_contents($jsonFile, json_encode($data, JSON_PRETTY_PRINT));
+   echo $data['likes'];
+}
