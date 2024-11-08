df5c974a75e8e6b413f2961a610a1133a0029808
Your Name <your_email@example.com>
2024-11-09 00:24:48 +0900

    google map embedded ( TODO: q)

--- story.php
+++ story.php
@@ -52,7 +52,18 @@ $maxId = $storyData->getMaxId();
         </div>
         <!-- TODO: 地図のコンテンツを作成する(PHP) -->
         <div class="story-article-map">
-            <div class="story-article-map-content"></div>
+            <div class="story-article-map-content">
+                <iframe
+                    width="600"
+                    height="450"
+                    style="border:0"
+                    loading="lazy"
+                    allowfullscreen
+                    referrerpolicy="no-referrer-when-downgrade"
+                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAvCim13FGe1rH23MIoU3ARs45Ngx6z218
+    &q=Space+Needle,Seattle+WA">
+                </iframe>
+            </div>
             <div class="story-article-map-title"><?php echo $story->location ?></div>
         </div>
     </article>
