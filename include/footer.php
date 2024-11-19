f00b0a11690815f67b2ba092e7ef31dad0a73ced
liuchiawei <doublecheap@gmail.com>
2024-11-19 10:42:42 +0900

    logoの実装

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,8 @@
 <footer class="footer">
   <a href="index.php">
-    <div class="footer-logo"></div>
+    <div class="footer-logo">
+      <img src="image/logo.svg" alt="Deep新宿">
+    </div>
   </a>
   <div class="copyright">
     <p>©2024 コードの森 All rights reserved.</p>
