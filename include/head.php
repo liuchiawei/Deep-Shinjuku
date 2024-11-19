12044a5a393f47ba5776f60422e436abbd5d7b15
liuchiawei <doublecheap@gmail.com>
2024-11-19 10:50:53 +0900

    website description

--- include/head.php
+++ include/head.php
@@ -3,4 +3,6 @@
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
     <link rel="icon" href="image/favicon.ico">
     <title>DEEP SHINJUKU - 新宿怪談集</title>
+    <meta name="description" content="新宿の怪談を集めたサイトです。">
+    <meta name="keywords" content="新宿,怪談,新宿怪談,新宿の怪談,新宿の怪談集,新宿の怪談集め,新宿の怪談集めた,新宿の怪談集めたサイト,新宿の怪談集めたサイトです">
 </head>
