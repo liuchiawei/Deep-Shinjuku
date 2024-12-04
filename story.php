29ca7a83da7e3a8d2bce4981d7999ac3466b1891
liuchiawei <doublecheap@gmail.com>
2024-12-04 12:50:51 +0900

    story-comment-form-post-author 文字の色の修正

--- story.php
+++ story.php
@@ -102,7 +102,7 @@ if (isset($_POST['hasLiked'])) {
                     <form id="commentForm" data-story-id="<?php echo $story->id; ?>" action="story.php?id=<?php echo $story->id; ?>" method="POST">
                         <textarea name="content" id="content" cols="30" rows="10" placeholder="コメント" required></textarea>
                         <div class="story-comment-form-post">
-                            <input type="text" name="author" id="author" placeholder="名前">
+                            <input class="story-comment-form-post-author" type="text" name="author" id="author" placeholder="名前">
                             <button type="submit" class="story-comment-btn">投稿</button>
                         </div>
                     </form>
