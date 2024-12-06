b66deb5173dd802cd3c50776f9729408bcd5cd34
liuchiawei <doublecheap@gmail.com>
2024-12-06 12:17:03 +0900

    <head>に viewport, charset の追加

--- include/head.php
+++ include/head.php
@@ -1,4 +1,6 @@
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
     <link rel="icon" href="image/favicon.ico">
