b3c2de6a49baf4cc8dffd854ca201dceaee39890
liuchiawei <doublecheap@gmail.com>
2024-12-15 21:02:17 +0900

    update og value

--- include/head.php
+++ include/head.php
@@ -10,9 +10,6 @@
     <meta property="og:title" content="DEEP SHINJUKU - 新宿怪談集">
     <meta property="og:description" content="新宿の怪談を集めたサイトです。">
     <meta property="og:image" content="image/background/intro.jpg">
-    <meta property="og:image:alt" content="新宿の怪談を集めたサイトです。">
-    <meta property="og:image:width" content="436">
-    <meta property="og:image:height" content="228">
     <meta property="og:url" content="https://deepshinjuku.com/urbanmyth/">
     <meta property="og:type" content="website">
 </head>
