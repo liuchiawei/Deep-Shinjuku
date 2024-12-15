19c794fc5068a5f985b92c195d90995e3694d21f
liuchiawei <doublecheap@gmail.com>
2024-12-15 20:52:30 +0900

    head <meta property>

--- include/head.php
+++ include/head.php
@@ -7,4 +7,12 @@
     <title>DEEP SHINJUKU - 新宿怪談集</title>
     <meta name="description" content="新宿の怪談を集めたサイトです。">
     <meta name="keywords" content="新宿,怪談,新宿怪談,新宿の怪談,新宿の怪談集,新宿の怪談集め,新宿の怪談集めた,新宿の怪談集めたサイト,新宿の怪談集めたサイトです">
+    <meta property="og:title" content="DEEP SHINJUKU - 新宿怪談集">
+    <meta property="og:description" content="新宿の怪談を集めたサイトです。">
+    <meta property="og:image" content="image/background/intro.jpg">
+    <meta property="og:image:alt" content="新宿の怪談を集めたサイトです。">
+    <meta property="og:image:width" content="436">
+    <meta property="og:image:height" content="228">
+    <meta property="og:url" content="https://deepshinjuku.com/urbanmyth/">
+    <meta property="og:type" content="website">
 </head>
